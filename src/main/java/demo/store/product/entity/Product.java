0e2e8fd1d192f104cbdace7abfc415db0f8e689b
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-05-12 12:21:49 -0500

    Servicio rest Producto

--- src/main/java/demo/store/product/entity/Product.java
+++ src/main/java/demo/store/product/entity/Product.java
@@ -1,11 +1,15 @@
 package demo.store.product.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.Date;
 
 @Entity
@@ -18,8 +22,12 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty(message = "El nombre no debe ser vacío")
     private String name;
     private String description;
+
+    @Positive(message = "El stock debe ser mayor que cero")
     private Double stock;
     private Double price;
     private String status;
@@ -27,8 +35,10 @@ public class Product {
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
 
+    @NotNull(message = "La categoria no puede ser vacia")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
     private Category category;
 
 
